b4265d111cc3021159d7a54d8a508fa3f581adac
EC2 Default User <ec2-user@ip-172-31-2-95.ec2.internal>
2022-06-13 09:10:20 +0000

    1st commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # 顧客用
+  # /customers/sign_in
+  devise_for :customer, skip: [:password,], controllers: {
+    registrations: "customer/registrations",
+    sessions: "customer/sessions"
+  }
+  devise_for :admins, skip:[:registrations, :password,], controllers: {
+    sessions: "admin/sessions"
+  }
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
