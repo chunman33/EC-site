b5b7673980da019cf26d0982ad137fdb35512ba1
EC2 Default User <ec2-user@ip-172-31-2-95.ec2.internal>
2022-08-12 10:15:08 +0000

    seedに初期データを登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#管理者ログイン情報
 Admin.create!(
    email: "a@a",
    password: "aaaaaa"
 )
+
+
+#一つずつ作成
+Genre.create(name: "ケーキ")
+Genre.create(name: "プリン")
+
+Genres = Genre.create!([
+    {name: "焼き菓子"}, 
+    {name: "チョコ"},
+    {name: "タルト"}
+])
+
+
+
+
+5.times do |n|
+ Item.create!(
+   name: "test#{n+1}",
+   description: "testtesttest#{n+1}",
+   sale_status: "true",
+   genre_id: Genre.find(n+1).id,
+   excluding_tax_price: 1000,
+   )
+end
+
+Item.create!(name: "販売停止テスト", description: "販売停止の挙動をテストします。", sale_status: "false", genre_id: Genre.find(2).id, excluding_tax_price: 1000)
+
