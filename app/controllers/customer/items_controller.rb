c4c9f7ca9f28d5e0082b22fa719e1522b3a913ac
EC2 Default User <ec2-user@ip-172-31-47-83.ap-northeast-1.compute.internal>
2022-10-08 16:30:38 +0000

    ジャンル検索機能実装

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,7 +1,11 @@
 class Customer::ItemsController < ApplicationController
   def index
     @items = Item.all
-    @genres = Genre.all
+     @genres = Genre.all
+    if params[:genre_id] # items.index.erbのlink_toから渡ってきたgenre_idパラメータを取得した場合の処理
+      @genre = Genre.find(params[:genre_id])# @genreにgenreモデルからgenreのidカラムをパラメータで取得して代入
+      @items = @genre.Item.all # @genreに紐づくitemモデルの商品全部を@itemsに代入
+    end
   end
   
   def show
